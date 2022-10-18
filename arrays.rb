4ec808dc31388aa3700fc081471254b0278182ec
Caleb Thomas <cjthomas00@comcast.net>
2022-10-18 14:39:41 -0600

    Initial commit.

---
+++ arrays.rb
@@ -0,0 +1,4 @@
+baby_names = ["Lyla", "Scarlett", "Olivia", "Blake"]
+siblings_ages = [25, 30, 31, 32]
+monthly_bills = [550.47, 1536.76, 145.09, 202.98]
+assignments_passed = [true, false, true, true]
